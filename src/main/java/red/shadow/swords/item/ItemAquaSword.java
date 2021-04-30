4d1cb78f6cfb1b9d7dac3c65bc987723f26ff8ac
RedShadowGH <Admin@192.168.1.12>
2021-04-30 09:13:46 +0200

    Removal of SML placeholder mods, Added Aqua Sword BETA 1

---
+++ src/main/java/red/shadow/swords/item/ItemAquaSword.java
@@ -0,0 +1,81 @@
+
+package red.shadow.swords.item;
+
+import red.shadow.swords.procedure.ProcedureAquaSwordToolInHandTick;
+import red.shadow.swords.creativetab.TabSwords;
+import red.shadow.swords.ElementsSwordPlusMod;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.google.common.collect.Multimap;
+
+@ElementsSwordPlusMod.ModElement.Tag
+public class ItemAquaSword extends ElementsSwordPlusMod.ModElement {
+	@GameRegistry.ObjectHolder("sword_plus:aqua_sword")
+	public static final Item block = null;
+	public ItemAquaSword(ElementsSwordPlusMod instance) {
+		super(instance, 68);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemSword(EnumHelper.addToolMaterial("AQUA_SWORD", 0, 970, 2.5f, 0f, 2)) {
+			@Override
+			public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot slot) {
+				Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
+				if (slot == EntityEquipmentSlot.MAINHAND) {
+					multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+							new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double) this.getAttackDamage(), 0));
+					multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+							new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -3, 0));
+				}
+				return multimap;
+			}
+
+			public Set<String> getToolClasses(ItemStack stack) {
+				HashMap<String, Integer> ret = new HashMap<String, Integer>();
+				ret.put("sword", 0);
+				return ret.keySet();
+			}
+
+			@Override
+			public void onUpdate(ItemStack itemstack, World world, Entity entity, int slot, boolean par5) {
+				super.onUpdate(itemstack, world, entity, slot, par5);
+				int x = (int) entity.posX;
+				int y = (int) entity.posY;
+				int z = (int) entity.posZ;
+				if (entity instanceof EntityLivingBase && ((EntityLivingBase) entity).getHeldItemMainhand().equals(itemstack)) {
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					ProcedureAquaSwordToolInHandTick.executeProcedure($_dependencies);
+				}
+			}
+		}.setUnlocalizedName("aqua_sword").setRegistryName("aqua_sword").setCreativeTab(TabSwords.tab));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("sword_plus:aqua_sword", "inventory"));
+	}
+}
---
+++ src/main/java/red/shadow/swords/procedure/ProcedureAquaSwordToolInHandTick.java
@@ -0,0 +1,27 @@
+package red.shadow.swords.procedure;
+
+import red.shadow.swords.ElementsSwordPlusMod;
+
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.init.MobEffects;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+@ElementsSwordPlusMod.ModElement.Tag
+public class ProcedureAquaSwordToolInHandTick extends ElementsSwordPlusMod.ModElement {
+	public ProcedureAquaSwordToolInHandTick(ElementsSwordPlusMod instance) {
+		super(instance, 68);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure AquaSwordToolInHandTick!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof EntityLivingBase)
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.SPEED, (int) 100, (int) 1));
+	}
+}
