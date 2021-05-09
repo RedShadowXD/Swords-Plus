d70ccdc3556ae308f5d3ba2eb6e0de2f003be9b0
RedShadowGH <Admin@192.168.1.12>
2021-05-09 16:08:54 +0200

    Saber Sword BETA-2

--- src/main/java/red/shadow/swords/item/ItemSaberSword.java
+++ src/main/java/red/shadow/swords/item/ItemSaberSword.java
@@ -1,7 +1,6 @@
 
 package red.shadow.swords.item;
 
-import red.shadow.swords.procedure.ProcedureSaberSwordLivingEntityIsHitWithTool;
 import red.shadow.swords.creativetab.TabSwords;
 import red.shadow.swords.ElementsSwordPlusMod;
 
@@ -12,18 +11,15 @@ import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 
-import net.minecraft.world.World;
 import net.minecraft.item.ItemSword;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
 import java.util.Set;
-import java.util.Map;
 import java.util.HashMap;
 
 import com.google.common.collect.Multimap;
@@ -55,24 +51,6 @@ public class ItemSaberSword extends ElementsSwordPlusMod.ModElement {
 				HashMap<String, Integer> ret = new HashMap<String, Integer>();
 				ret.put("sword", 1);
 				return ret.keySet();
-			}
-
-			@Override
-			public boolean hitEntity(ItemStack itemstack, EntityLivingBase entity, EntityLivingBase entity2) {
-				super.hitEntity(itemstack, entity, entity2);
-				int x = (int) entity.posX;
-				int y = (int) entity.posY;
-				int z = (int) entity.posZ;
-				World world = entity.world;
-				{
-					Map<String, Object> $_dependencies = new HashMap<>();
-					$_dependencies.put("x", x);
-					$_dependencies.put("y", y);
-					$_dependencies.put("z", z);
-					$_dependencies.put("world", world);
-					ProcedureSaberSwordLivingEntityIsHitWithTool.executeProcedure($_dependencies);
-				}
-				return true;
 			}
 		}.setUnlocalizedName("saber_sword").setRegistryName("saber_sword").setCreativeTab(TabSwords.tab));
 	}
